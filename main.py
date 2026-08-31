ef30ace5f945b0190de5cba06bab9837018f46aa
v9418kumar-tech <v9418kumar@gmail.com>
2026-08-31 22:45:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import yfinance as yf
 from datetime import datetime
+import requests
+
+TELEGRAM_TOKEN = "8908511972:AAFWn-0KKs6YkyFPyrhZpX7nKDUE87g9u8Y"
+CHAT_ID = "7416362918"
+
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    requests.get(
+        url,
+        params={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+    )
 
 
 def scanner():
@@ -10,6 +26,7 @@ def scanner():
     try:
         with open("stocks.txt", "r") as f:
             stocks = [x.strip() for x in f if x.strip()]
+
     except:
         print("stocks.txt not found")
         return
@@ -18,7 +35,6 @@ def scanner():
     for symbol in stocks:
 
         try:
-
             data = yf.download(
                 symbol,
                 period="2d",
@@ -26,7 +42,6 @@ def scanner():
                 progress=False,
                 auto_adjust=False
             )
-
 
             if len(data) < 3:
                 continue
@@ -36,44 +51,37 @@ def scanner():
             previous = data.iloc[-3]
 
 
-            current_open = float(current["Open"].item())
-            current_close = float(current["Close"].item())
+            current_open = float(current["Open"])
+            current_close = float(current["Close"])
 
-            previous_open = float(previous["Open"].item())
-            previous_close = float(previous["Close"].item())
-
-
-            current_volume = int(current["Volume"].item())
-            previous_volume = int(previous["Volume"].item())
+            previous_open = float(previous["Open"])
+            previous_close = float(previous["Close"])
 
 
-            # Current candle Red
+            current_volume = int(current["Volume"])
+            previous_volume = int(previous["Volume"])
+
+
             red = current_close < current_open
-
-
-            # Previous candle Green
             green = previous_close > previous_open
-
-
-            # Current volume greater than previous volume
             volume = current_volume > previous_volume
 
 
             if red and green and volume:
 
-                print(
-                    "🚨 ALERT",
-                    symbol,
-                    "| Current Volume:",
-                    current_volume,
-                    "| Previous Volume:",
-                    previous_volume
+                message = (
+                    f"🚨 ALERT {symbol}\n"
+                    f"Current Volume: {current_volume}\n"
+                    f"Previous Volume: {previous_volume}"
                 )
+
+                print(message)
+
+                send_telegram(message)
 
 
         except Exception:
             continue
-
 
 
 scanner()
