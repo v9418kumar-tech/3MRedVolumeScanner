8a811822a921250bcd64c61ee9d9f3c79b5c0228
v9418kumar-tech <v9418kumar@gmail.com>
2026-08-31 17:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import yfinance as yf
 from datetime import datetime
 
-
 def scanner():
 
     now = datetime.now().strftime("%H:%M:%S")
@@ -20,7 +19,6 @@ def scanner():
     for symbol in stocks:
 
         try:
-
             data = yf.download(
                 symbol,
                 period="1d",
@@ -34,26 +32,24 @@ def scanner():
             data["Volume_Avg"] = data["Volume"].rolling(5).mean()
 
             last_volume = data["Volume"].iloc[-1]
-avg_volume = data["Volume_Avg"].iloc[-1]
+            avg_volume = data["Volume_Avg"].iloc[-1]
 
-if hasattr(last_volume, "iloc"):
-    last_volume = last_volume.iloc[0]
+            if hasattr(last_volume, "iloc"):
+                last_volume = last_volume.iloc[0]
 
-if hasattr(avg_volume, "iloc"):
-    avg_volume = avg_volume.iloc[0]
+            if hasattr(avg_volume, "iloc"):
+                avg_volume = avg_volume.iloc[0]
 
-last_volume = float(last_volume)
-avg_volume = float(avg_volume)
+            last_volume = float(last_volume)
+            avg_volume = float(avg_volume)
 
             if last_volume > avg_volume * 3:
-
                 result.append(
                     f"{symbol} Volume Spike {int(last_volume)}"
                 )
 
         except Exception as e:
             print(symbol, e)
-
 
     message = f"""
 🚨 5 Min Volume Scanner
@@ -66,7 +62,6 @@ Time: {now}
         message += "\n".join(result)
     else:
         message += "No Volume Spike Found"
-
 
     print(message)
 
