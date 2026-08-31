6581cf28c58327d7a6b141b3aa1fc08634383ec3
v9418kumar-tech <v9418kumar@gmail.com>
2026-08-31 20:47:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def scanner():
     for symbol in stocks:
 
         try:
+
             data = yf.download(
                 symbol,
                 period="2d",
@@ -31,31 +32,40 @@ def scanner():
             if data.empty or len(data) < 2:
                 continue
 
+
             current = data.iloc[-1]
             previous = data.iloc[-2]
 
-            current_open = float(current["Open"])
-            current_close = float(current["Close"])
-            previous_open = float(previous["Open"])
-            previous_close = float(previous["Close"])
 
-            current_volume = int(current["Volume"])
-            previous_volume = int(previous["Volume"])
+            current_open = float(current["Open"].iloc[0])
+            current_close = float(current["Close"].iloc[0])
+
+            previous_open = float(previous["Open"].iloc[0])
+            previous_close = float(previous["Close"].iloc[0])
+
+
+            current_volume = int(current["Volume"].iloc[0])
+            previous_volume = int(previous["Volume"].iloc[0])
+
 
             # Current candle Red
             red_candle = current_close < current_open
 
+
             # Previous candle Green
             green_previous = previous_close > previous_open
+
 
             # Current volume greater than previous volume
             volume_condition = current_volume > previous_volume
 
+
             if red_candle and green_previous and volume_condition:
 
                 result.append(
-                    f"{symbol} ALERT | Current Vol: {current_volume} | Previous Vol: {previous_volume}"
+                    f"{symbol} ALERT | Current Volume: {current_volume} | Previous Volume: {previous_volume}"
                 )
+
 
         except Exception as e:
             print(symbol, e)
@@ -64,6 +74,7 @@ def scanner():
     print("\n🚨 5 Min Red Green Volume Scanner")
 
     print("Time:", now)
+
 
     if result:
         for r in result:
