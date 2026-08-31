bde32468c9f7baf9c45b23912ea3a9d70051a0e6
v9418kumar-tech <v9418kumar@gmail.com>
2026-08-31 16:56:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,64 @@
-import requests
-from datetime import datetime
 import yfinance as yf
-
-
-# Telegram details (baad me bharenge)
-BOT_TOKEN = ""
-CHAT_ID = ""
-
-
-def send_message(msg):
-    if BOT_TOKEN and CHAT_ID:
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-        requests.post(url, data={
-            "chat_id": CHAT_ID,
-            "text": msg
-        })
-
+import pandas as pd
+from datetime import datetime
 
 def scanner():
 
     now = datetime.now().strftime("%H:%M:%S")
 
+    stocks = [
+        "RELIANCE.NS",
+        "TATAMOTORS.NS",
+        "HDFCBANK.NS",
+        "ICICIBANK.NS",
+        "SBIN.NS"
+    ]
+
+    result = []
+
     print("5 Minute Volume Scanner Running", now)
 
-    symbol = "RELIANCE.NS"
+    for symbol in stocks:
 
-    data = yf.download(
-        symbol,
-        period="1d",
-        interval="5m"
-    )
+        try:
+            data = yf.download(
+                symbol,
+                period="1d",
+                interval="5m",
+                progress=False
+            )
 
-    print(data.tail())
+            if len(data) < 5:
+                continue
+
+            data["Volume_Avg"] = data["Volume"].rolling(5).mean()
+
+            last = data.iloc[-1]
+
+            volume = float(last["Volume"])
+            avg_volume = float(last["Volume_Avg"])
+
+            if volume > avg_volume * 3:
+                result.append(
+                    f"{symbol} Volume Spike {volume:.0f}"
+                )
+
+        except Exception as e:
+            print(symbol, e)
 
     message = f"""
-5 Min Volume Scanner
+🚨 5 Min Volume Scanner
 
 Time: {now}
 
-Stock: {symbol}
-
-Yahoo Finance Data Connected ✅
 """
 
+    if result:
+        message += "\n".join(result)
+    else:
+        message += "No Volume Spike Found"
+
     print(message)
-    send_message(message)
 
 
 scanner()
